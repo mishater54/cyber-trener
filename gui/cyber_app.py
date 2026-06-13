605fe09c403216d742d6776db766409c2fa66fa9
Mykhailo Shulika <258686@edu.p.lodz.pl>
2026-06-14 00:59:00 +0200

    Added record page

--- gui/cyber_app.py
+++ gui/cyber_app.py
@@ -2,6 +2,8 @@ import customtkinter as ctk
 
 from home_page import HomePage
 from history_page import HistoryPage
+from record_page import RecordsPage
+from stats_page import StatsPage
 
 class CyberApp(ctk.CTk):
     def __init__(self):
@@ -16,23 +18,28 @@ class CyberApp(ctk.CTk):
         
         ctk.CTkButton(self.sidebar, text="Home", command=lambda: self.switch_page("home"), corner_radius=20).pack(pady=10, padx=20, fill="x")
         ctk.CTkButton(self.sidebar, text="History", command=lambda: self.switch_page("history"), corner_radius=20).pack(pady=10, padx=20, fill="x")
-        ctk.CTkButton(self.sidebar, text="Records", command=lambda: self.switch_page("records"), corner_radius=20).pack(pady=10, padx=20, fill="x")
         ctk.CTkButton(self.sidebar, text="Stats", command=lambda: self.switch_page("stats"), corner_radius=20).pack(pady=10, padx=20, fill="x")
+        ctk.CTkButton(self.sidebar, text="Records", command=lambda: self.switch_page("records"), corner_radius=20).pack(pady=10, padx=20, fill="x")
 
         self.main_container = ctk.CTkFrame(self, corner_radius=15)
         self.main_container.pack(side="right", expand=True, fill="both", padx=20, pady=20)
 
         self.pages = {
             "home": HomePage(self.main_container),
-            "history": HistoryPage(self.main_container)
+            "history": HistoryPage(self.main_container),
+            "stats": StatsPage(self.main_container),
+            "records": RecordsPage(self.main_container)
         }
         
         self.switch_page("home")
 
     def switch_page(self, page_name):
-        for page in self.pages.values():
-            page.pack_forget()
-        self.pages[page_name].pack(fill="both", expand=True)
+        if page_name in self.pages:
+            for page in self.pages.values():
+                page.pack_forget()
+            self.pages[page_name].pack(fill="both", expand=True)
+        else:
+            print(f"Page '{page_name}' is not implemented yet!")
 
 if __name__ == "__main__":
     app = CyberApp()
